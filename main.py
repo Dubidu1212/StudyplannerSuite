be39948499b89930a4185287677d0ece61849b1a
Raphael Zumbrunn <me@zura.ch>
2022-10-13 20:11:25 +0000

    fixed iteration

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from CoursePlaner.Backend.scheduletest import *
 courses = get_courses_format(["551-1402-00L","551-0307-00L","551-0307-01L", "401-2813-00L","401-0241-00L"],"2022W")
 
 for course in courses:
-    for module in course:
+    for module in course.modules:
         print(module)
